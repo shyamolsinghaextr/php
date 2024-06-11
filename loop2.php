1e26423b96f7ae4a03e73a5f38efd3add5db7445
shyamolsinghaextr <shyamolsingha3@gmail.com>
2024-06-11 21:30:18 +0600

    loop class 2

--- loop2.php
+++ loop2.php
@@ -10,11 +10,28 @@
 	
 	<?php
 	
+		$i = 100;
+		while ($i <= 10) {
+		   echo 'This is While Loop <br>';
+		   $i++;
+		}	
+	
+		$i = 100;
+		do{
+		  echo 'This is Do While Loop <br>';
+		  $i++;
+		} while ($i <= 10);
+		
+		
+		for ($i = 5; $i <= 10; $i++) {
+		  echo "The number is: $i <br>";
+		}
+		
 		$somor = array("somor singha", "bhanubil", "01725478");
+		
 		foreach ($somor as $x) {
 		  echo "$x <br>";
 		}
-		
 	?>
 
 
