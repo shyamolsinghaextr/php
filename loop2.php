4ac50e2aeaccb264b048bd93dc81ee5813ee51ac
shyamolsinghaextr <shyamolsingha3@gmail.com>
2024-06-11 21:27:30 +0600

    loopclass2

---
+++ loop2.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<title>bootstrap</title>
+  </head>
+  <body>
+	
+	<?php
+	
+		$somor = array("somor singha", "bhanubil", "01725478");
+		foreach ($somor as $x) {
+		  echo "$x <br>";
+		}
+		
+	?>
+
+
+	<script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
